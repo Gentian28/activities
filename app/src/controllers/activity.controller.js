8328a1164346deb2a1b55fdd4613fe2679033979
Gentian Shkembi <gentian.shkembi@gmail.com>
2020-01-23 16:52:05 +0100

    test commit

--- app/src/controllers/activity.controller.js
+++ app/src/controllers/activity.controller.js
@@ -16,6 +16,7 @@ function createActivity(req, res, next) {
 }
 
 function getAllActivities(req, res, next) {
+    console.log('hitted')
     activityService.getAllActivities()
         .then(activities => res.json(activities))
         .catch(err => next(err));
