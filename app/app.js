53a520e6e6377484a49a92c9e840181bf4ef9da4
Gentian Shkembi <gentian.shkembi@gmail.com>
2020-01-23 15:35:00 +0100

    Check db connection

--- app/app.js
+++ app/app.js
@@ -23,10 +23,18 @@ app.get('/api/activity', (req, res) => {
     res.json({ "message": "from activity api" });
 });
 
+try {
+    mongoose.connect(config.mongodb.dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }, () => {
+        console.log('Conected to MongoDB');
+    })
+} catch (error) {
+    console.log(error);
+    handleError(error);
+}
 // connect to mongodb
-mongoose.connect(config.mongodb.dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }, () => {
-    console.log('Conected to MongoDB');
-})
+// mongoose.connect(config.mongodb.dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }, () => {
+//     console.log('Conected to MongoDB');
+// })
 
 // setup routes
 app.use('/api', activityRoutes);
